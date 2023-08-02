055d95d2e3718e628de5b834b1f26629707340f7
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-02 11:49:53 -0400

    adding launch date and launch success

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -1,7 +1,18 @@
+import dayjs from "dayjs"
+import "dayjs/locale/es"
+
 function Launches(data) {
     return(
-        <div className="mt-3 bg-amber-400 w-96 p-4 rounded-md text-center text-lg cursor-pointer">
-            <h1>{data.data.mission_name} </h1>
+        <div className="mt-3 bg-amber-400 w-96 p-4 rounded-md text-lg cursor-pointer">
+            <span>Mission <strong>{data.data.mission_name}</strong></span>
+            <span className={`float-right ${data.data.launch_success ? 'bg-green-500' : 'bg-red-500'} p-1 rounded-md`}>{data.data.launch_success? "Sucees" : "Failure"}</span>
+            <div>
+                <span>
+                    {dayjs (data.data.launch_date_local)
+                    .locale("es")
+                    .format("DD/MM/YYYY")}
+                </span>
+            </div>
         </div>
     )
 }
