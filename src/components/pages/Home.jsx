5a2ffeaf331deea311f6bca5ae22a9a783f5684c
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-01 21:42:46 -0400

    style and NavLink on home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,13 @@
+import { NavLink } from "react-router-dom"
+
 function Home() {
-    return (
-        <div> Welcome! </div>
+
+    return(
+        <div className="flex flex-col items-center text-center mt-56">
+            <h1  className="text-3xl">Wekcome!</h1>
+            <p className="mt-2 text-base">this website is to see <a target="_blank" href="https://www.spacex.com" rel="noreferrer" className="underline underline-offset-4">Space X</a> launches</p>
+            <span className="text-lg rounded-lg bg-cyan-600 p-2 mt-5 cursor-pointer"> <NavLink to='/launches'>see lauches</NavLink></span>
+        </div>
     )
 }
 
