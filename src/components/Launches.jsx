fdd94b1993a45cac331c8cac11954b4e93ab6b92
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-02 08:29:32 -0400

    styles for Launch and Launches

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -1,7 +1,7 @@
 function Launches(data) {
     return(
-        <div className="felx flex-col">
-            <h1> launches name is: {data.data.mission_name} </h1>
+        <div className="mt-3 bg-amber-400 w-96 p-4 rounded-md text-center text-lg cursor-pointer">
+            <h1>{data.data.mission_name} </h1>
         </div>
     )
 }
--- src/components/pages/Launch.jsx
+++ src/components/pages/Launch.jsx
@@ -1,7 +1,21 @@
+import { Launches } from "../Launches"
+import { UseGetDataLaunches } from "../../hooks/UseGetData"
+
 function Launch() {
+
+    const { launch } = UseGetDataLaunches()
+
     return (
-        <div> Launch </div>
+        <div>
+            <div className="flex justify-center">
+                <h1 className="text-3xl bg-cyan-600 p-3 mt-10 rounded-md">SpaceX Launches</h1>
+            </div>
+
+            <div className="grid justify-items-center w-full grid-cols-1 mt-8 mb-10 md:grid-cols-2">
+                {launch?.map(items => <Launches key={items.flight_number} data={items}/>)}
+            </div>
+        </div>
     )
 }
 
-export { Launch }
+export { Launch } 
