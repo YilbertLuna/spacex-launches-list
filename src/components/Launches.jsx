0f96b93926e4f38c0e98eab5b7b075392980227b
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-03 20:58:51 -0400

    adding react icons and calendar icon

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -1,13 +1,15 @@
 import dayjs from "dayjs"
 import "dayjs/locale/es"
+import { FcCalendar } from "react-icons/fc"
 
 function Launches(data) {
     return(
-        <div className="mt-3 bg-amber-400 w-96 p-4 rounded-md text-lg cursor-pointer">
+        <div className="mt-3 bg-amber-400 w-96 p-4 rounded-md text-lg">
             <span>Mission <strong>{data.data.mission_name}</strong></span>
             <span className={`float-right ${data.data.launch_success ? 'bg-green-500' : 'bg-red-500'} p-1 rounded-md`}>{data.data.launch_success? "Sucees" : "Failure"}</span>
-            <div>
-                <span>
+            <div className="grid">
+                <span className="flex items-center">
+                    <FcCalendar className="mr-2"/>
                     {dayjs (data.data.launch_date_local)
                     .locale("es")
                     .format("DD/MM/YYYY")}
