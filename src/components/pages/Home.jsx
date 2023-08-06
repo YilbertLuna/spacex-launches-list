79d477e95e6571cfeb4db4257af5d1fd43ac8889
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-06 18:40:11 -0400

    modified file

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Home() {
 
@@ -6,7 +6,7 @@ function Home() {
         <div className="flex flex-col items-center text-center mt-56">
             <h1  className="text-3xl">Wekcome!</h1>
             <p className="mt-2 text-base">this website is to see <a target="_blank" href="https://www.spacex.com" rel="noreferrer" className="underline underline-offset-4">Space X</a> launches</p>
-            <span className="text-lg rounded-lg bg-cyan-600 p-2 mt-5 cursor-pointer"> <NavLink to='/launches'>see lauches</NavLink></span>
+            <span className="text-lg rounded-lg bg-cyan-600 p-2 mt-5 cursor-pointer"> <Link to='/launches'>see lauches</Link></span>
         </div>
     )
 }
