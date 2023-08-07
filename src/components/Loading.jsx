40ed851784d315896690eb154fbc79d9567c2a35
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-07 09:36:40 -0400

    file loading and not found

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+    return(
+        <div className="flex flex-col items-center mt-8">
+            <p className="items-center text-2xl">Loading...</p>
+        </div>
+    )
+}
+
+export { Loading }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return(
+        <div className="flex w-full justify-center mt-8">
+            <p className="text-2xl">something went wrong, we couldnt get the data</p>
+        </div>
+    )
+}
+
+export { NotFound }
